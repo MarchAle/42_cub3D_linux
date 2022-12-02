847196b46a11888a842eb831848c89bd31b59648
amarchal <aleximarchal@hotmail.com>
2022-12-02 09:26:10 +0100

    monster movement ok

--- srcs/monster.c
+++ srcs/monster.c
@@ -39,19 +39,39 @@ void    ft_free_monster_map(t_cub *cub)
     free(cub->monster_map);
 }
 
+
+void	print_map(t_cub *cub)
+{
+	int i = 0;
+	// int j;
+	while (cub->map[i])
+	{
+			printf("%s", cub->map[i]);
+		// j = 0;
+		// while (cub->map[i][j])
+		// {
+		// 	write(2, &cub->map[i][j], 1);
+		// 	j++;
+		// }
+		i++;
+	}
+}
+
 void    ft_move_monster(t_cub *cub)
 {
     t_monster *monster = cub->monsters;
     while (monster)
     {
         float   old_x = monster->x;
-        if (cub->map[(int)(monster->y + MONSTER_STEP)][(int)(monster->x)] == '0' || cub->map[(int)(monster->y + MONSTER_STEP)][(int)(monster->x)] == 'X')
+        if (cub->map[(int)(monster->y)][(int)(monster->x + MONSTER_STEP)] == '0' || cub->map[(int)(monster->y)][(int)(monster->x + MONSTER_STEP)] == 'X')
         {
-            print_monster_map(cub);
+            // print_monster_map(cub);
+            // print_map(cub);
+            // printf("%c\n", cub->map[(int)(monster->y)][(int)(monster->x + MONSTER_STEP)]);
             monster->x += MONSTER_STEP;
             if ((int)old_x != (int)monster->x)
             {
-                printf("old_x %f x = %f\n\n", old_x, monster->x);
+                // printf("old_x %f x = %f\n\n", old_x, monster->x);
                 cub->map[(int)monster->y][(int)old_x] = '0';
                 cub->map[(int)monster->y][(int)monster->x] = 'X';
                 ft_free_monster_map(cub);
